72b7ed908388b268d6da0af209fb8d2d4580f8d1
Bin-Huang <tohuangbin@gmail.com>
2019-09-26 22:07:54 +0800

    Fix

--- src/prraypromise.ts
+++ src/prraypromise.ts
@@ -6,11 +6,11 @@ class PrrayPromise<T> extends Promise<Prray<T>> {
   constructor(props: (resolve: any, reject: any) => any) {
     super(props)
   }
-  mapAsync(mapper: any) {
-    return this.then(prray => prray.mapAsync(mapper))
+  mapAsync<U>(mapper): PrrayPromise<U> {
+    return prraypromise(this.then(prray => prray.mapAsync(mapper)))
   }
-  filterAsync(func: any) {
-    return this.then(prray => prray.filterAsync(func))
+  filterAsync(func: any): PrrayPromise<T> {
+    return prraypromise(this.then(prray => prray.filterAsync(func)))
   }
   reduceAsync(func: any, initialValue?: any) {
     return this.then(prray => prray.reduceAsync(func, initialValue))
@@ -18,26 +18,26 @@ class PrrayPromise<T> extends Promise<Prray<T>> {
   reduceRightAsync(func: any, initialValue?: any) {
     return this.then(prray => prray.reduceRightAsync(func, initialValue))
   }
-  sortAsync(func?: any) {
-    return this.then(prray => prray.sortAsync(func))
+  sortAsync(func?: any): PrrayPromise<T> {
+    return prraypromise(this.then(prray => prray.sortAsync(func)))
   }
-  findAsync(func: any) {
+  findAsync(func: any): Promise<T|undefined> {
     return this.then(prray => prray.findAsync(func))
   }
-  findIndexAsync(func: any) {
+  findIndexAsync(func: any): Promise<number> {
     return this.then(prray => prray.findIndexAsync(func))
   }
-  everyAsync(func: any) {
+  everyAsync(func: any): Promise<boolean> {
     return this.then(prray => prray.everyAsync(func))
   }
-  someAsync(func: any) {
+  someAsync(func: any): Promise<boolean> {
     return this.then(prray => prray.someAsync(func))
   }
   forEachAsync(func: any) {
     return this.then(prray => prray.forEachAsync(func))
   }
-  slice(start?: number, end?: number) {
-    return this.then(prray => prray.slice(start, end))
+  slice(start?: number, end?: number): PrrayPromise<T> {
+    return prraypromise(this.then(prray => prray.slice(start, end)))
   }
 }
 
@@ -49,3 +49,6 @@ function prraypromise<T>(promise: Promise<Prray<T>>) {
 }
 
 export { PrrayPromise, prraypromise }
+
+const p = new PrrayPromise(() => null)
+p.mapAsync
