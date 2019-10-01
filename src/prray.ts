559089392334ad4f35d846a686743e40213a4511
Bin-Huang <tohuangbin@gmail.com>
2019-10-01 21:14:53 +0800

    Update method types

--- src/prray.ts
+++ src/prray.ts
@@ -1,12 +1,19 @@
 import { prraypromise, PrrayPromise } from './prraypromise'
 import * as methods from './methods'
-import { IMapCallback, ITester, IReduceCallback } from './types'
 
-// TODO: Update method types
 // TODO: Mutable and Immutable
 // TODO: thisArg
 
 export class Prray<T> extends Array<T> {
+
+  static of<T>(...args: T[]): Prray<T> {
+    return Prray.from(args)
+  }
+
+  static isPrray(obj: any): boolean {
+    return obj instanceof Prray
+  }
+
   static from<T, U>(arrayLike: Iterable<T> | ArrayLike<T>): Prray<T>
   static from<T, U>(arrayLike: Iterable<T> | ArrayLike<T>, mapFunc: (v: T, ix: number) => U, thisArg?: any): Prray<U>
   static from<T, U>(
@@ -23,25 +30,22 @@ export class Prray<T> extends Array<T> {
     return prr
   }
 
-  static of<T>(...args: T[]): Prray<T> {
-    return Prray.from(args)
-  }
-
-  static isPrray(obj: any): boolean {
-    return obj instanceof Prray
-  }
-
+  constructor(length: number)
+  constructor(...args: T[])
   constructor(...args: any[]) {
     super(...args)
   }
-  mapAsync<U>(mapper: IMapCallback<T, U>): PrrayPromise<U> {
-    const promise = methods.map(this, mapper)
+
+  mapAsync<U>(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<U> | U): PrrayPromise<U> {
+    const promise = methods.map(this, func)
     return prraypromise(promise.then(arr => Prray.from(arr)))
   }
-  filterAsync(func: ITester<T>): PrrayPromise<T> {
+
+  filterAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<boolean> | boolean): PrrayPromise<T> {
     const promise = methods.filter(this, func)
     return prraypromise(promise.then(arr => Prray.from(arr)))
   }
+
   reduceAsync(callback: (accumulator: T, currentValue: T, index: number, array: Prray<T>) => Promise<T>): Promise<T>
   reduceAsync<U>(
     callback: (accumulator: U, currentValue: T, index: number, array: Prray<T>) => Promise<U>,
@@ -53,6 +57,7 @@ export class Prray<T> extends Array<T> {
   ): Promise<any> {
     return methods.reduce(this, callback, initialValue)
   }
+
   reduceRightAsync(
     callback: (accumulator: T, currentValue: T, index: number, array: Prray<T>) => Promise<T>,
   ): Promise<T>
@@ -66,52 +71,65 @@ export class Prray<T> extends Array<T> {
   ): Promise<any> {
     return methods.reduceRight(this, callback, initialValue)
   }
-  sortAsync(func?: any): PrrayPromise<T> {
+
+  sortAsync(func?: (a: T, b: T) => Promise<number> | number): PrrayPromise<T> {
     const promise = methods.sort(this, func)
     return prraypromise(promise.then(arr => Prray.from(arr)))
   }
-  findAsync(func: ITester<T>): Promise<T> {
+
+  findAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<boolean> | boolean): Promise<T | undefined> {
     return methods.find(this, func)
   }
-  findIndexAsync(func: ITester<T>): Promise<number> {
+
+  findIndexAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<boolean> | boolean): Promise<number> {
     return methods.findIndex(this, func)
   }
-  everyAsync(func: ITester<T>): Promise<boolean> {
+
+  everyAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<boolean> | boolean): Promise<boolean> {
     return methods.every(this, func)
   }
-  someAsync(func: ITester<T>): Promise<boolean> {
+
+  someAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<boolean> | boolean): Promise<boolean> {
     return methods.some(this, func)
   }
-  forEachAsync<U>(func: IMapCallback<T, U>) {
+
+  forEachAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<any> | any) {
     return methods.forEach(this, func)
   }
+
   slice(start?: number, end?: number): Prray<T> {
     const result: T[] = super.slice(start, end)
     return _ensurePrray(result)
   }
 
-  map<U>(callback): Prray<U> {
-    return _ensurePrray(super.map(callback))
+  map<U>(func: (currentValue: T, index: number, prray: Prray<T>) => U): Prray<U> {
+    return _ensurePrray(super.map(func))
   }
-  filter(callback): Prray<T> {
-    return _ensurePrray(super.filter(callback))
+
+  filter(func: (currentValue: T, index: number, prray: Prray<T>) => boolean): Prray<T> {
+    return _ensurePrray(super.filter(func))
   }
+
   concat(...items: ConcatArray<T>[]): Prray<T>
   concat(...items: (ConcatArray<T> | T)[]): Prray<T>
   concat(...items: any[]): Prray<T> {
     return _ensurePrray(super.concat(...items))
   }
+
   reverse(): Prray<T> {
     return super.reverse() as Prray<T>
   }
+
   splice(start: number, deleteCount?: number, ...items: T[]): Prray<T> {
     // Why? If pass parameter deleteCount as undefined directly, the delete count will be zero actually :(
     const result = arguments.length >= 2 ? super.splice(start, deleteCount, ...items) : super.splice(start)
     return _ensurePrray(result)
   }
+
   toArray(): T[] {
     return [...this]
   }
+
 }
 
 export function prray<T>(arr: T[]): Prray<T> {
--- src/prraypromise.ts
+++ src/prraypromise.ts
@@ -1,15 +1,19 @@
 import { Prray } from './prray'
 
 class PrrayPromise<T> extends Promise<Prray<T>> {
-  constructor(props: (resolve: any, reject: any) => any) {
-    super(props)
+
+  constructor(executor: (resolve: (prray: Prray<T>) => any, reject: (err: Error) => any) => any) {
+    super(executor)
   }
-  mapAsync<U>(mapper): PrrayPromise<U> {
-    return prraypromise(this.then(prray => prray.mapAsync(mapper)))
+
+  mapAsync<U>(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<U> | U): PrrayPromise<U> {
+    return prraypromise(this.then(prray => prray.mapAsync(func)))
   }
-  filterAsync(func: any): PrrayPromise<T> {
+
+  filterAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<boolean> | boolean): PrrayPromise<T> {
     return prraypromise(this.then(prray => prray.filterAsync(func)))
   }
+
   reduceAsync(callback: (accumulator: T, currentValue: T, index: number, array: Prray<T>) => Promise<T>): Promise<T>
   reduceAsync<U>(
     callback: (accumulator: U, currentValue: T, index: number, array: Prray<T>) => Promise<U>,
@@ -21,6 +25,7 @@ class PrrayPromise<T> extends Promise<Prray<T>> {
   ): Promise<any> {
     return this.then(prray => prray.reduceAsync(callback, initialValue))
   }
+
   reduceRightAsync(
     callback: (accumulator: T, currentValue: T, index: number, array: Prray<T>) => Promise<T>,
   ): Promise<T>
@@ -34,101 +39,132 @@ class PrrayPromise<T> extends Promise<Prray<T>> {
   ): Promise<any> {
     return this.then(prray => prray.reduceRightAsync(callback, initialValue))
   }
-  sortAsync(func?: any): PrrayPromise<T> {
+
+  sortAsync(func?: (a: T, b: T) => Promise<number> | number): PrrayPromise<T> {
     return prraypromise(this.then(prray => prray.sortAsync(func)))
   }
-  findAsync(func: any): Promise<T | undefined> {
+
+  findAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<boolean> | boolean): Promise<T | undefined> {
     return this.then(prray => prray.findAsync(func))
   }
-  findIndexAsync(func: any): Promise<number> {
+
+  findIndexAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<boolean> | boolean): Promise<number> {
     return this.then(prray => prray.findIndexAsync(func))
   }
-  everyAsync(func: any): Promise<boolean> {
+
+  everyAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<boolean> | boolean): Promise<boolean> {
     return this.then(prray => prray.everyAsync(func))
   }
-  someAsync(func: any): Promise<boolean> {
+
+  someAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<boolean> | boolean): Promise<boolean> {
     return this.then(prray => prray.someAsync(func))
   }
-  forEachAsync(func: any) {
+
+  forEachAsync(func: (currentValue: T, index: number, prray: Prray<T>) => Promise<any> | any) {
     return this.then(prray => prray.forEachAsync(func))
   }
+
   slice(start?: number, end?: number): PrrayPromise<T> {
     return prraypromise(this.then(prray => prray.slice(start, end)))
   }
-  map<U>(callback): PrrayPromise<U> {
-    return prraypromise(this.then(prray => prray.map(callback)))
+
+  map<U>(func: (currentValue: T, index: number, prray: Prray<T>) => U): PrrayPromise<U> {
+    return prraypromise(this.then(prray => prray.map(func)))
   }
-  filter(callback): PrrayPromise<T> {
-    return prraypromise(this.then(prray => prray.filter(callback)))
+
+  filter(func: (currentValue: T, index: number, prray: Prray<T>) => boolean): PrrayPromise<T> {
+    return prraypromise(this.then(prray => prray.filter(func)))
   }
-  find(callback): Promise<T> {
-    return this.then(prray => prray.find(callback))
+
+  find(func: (currentValue: T, index: number, prray: Prray<T>) => boolean): Promise<T> {
+    return this.then(prray => prray.find(func))
   }
-  findIndex(callback): Promise<number> {
-    return this.then(prray => prray.findIndex(callback))
+
+  findIndex(func: (currentValue: T, index: number, prray: Prray<T>) => boolean): Promise<number> {
+    return this.then(prray => prray.findIndex(func))
   }
-  every(callback): Promise<boolean> {
-    return this.then(prray => prray.every(callback))
+
+  every(func: (currentValue: T, index: number, prray: Prray<T>) => boolean): Promise<boolean> {
+    return this.then(prray => prray.every(func))
   }
-  some(callback): Promise<boolean> {
-    return this.then(prray => prray.some(callback))
+
+  some(func: (currentValue: T, index: number, prray: Prray<T>) => boolean): Promise<boolean> {
+    return this.then(prray => prray.some(func))
   }
+
   includes(element: T, fromIndex?: number): Promise<boolean> {
     return this.then(prray => prray.includes(element, fromIndex))
   }
+
   indexOf(element: T, fromIndex?: number): Promise<number> {
     return this.then(prray => prray.indexOf(element, fromIndex))
   }
+
   lastIndexOf(element: T, fromIndex?: number): Promise<number> {
     return this.then(prray => {
       fromIndex = fromIndex === undefined ? prray.length - 1 : fromIndex // fix odd bug
       return prray.lastIndexOf(element, fromIndex)
     })
   }
+
   join(separator?: string): Promise<string> {
     return this.then(prray => prray.join(separator))
   }
+
   keys(): Promise<IterableIterator<number>> {
     return this.then(prray => prray.keys())
   }
+
   values(): Promise<IterableIterator<T>> {
     return this.then(prray => prray.values())
   }
+
   entries(): Promise<IterableIterator<[number, T]>> {
     return this.then(prray => prray.entries())
   }
+
   fill(value: T, start?: number, end?: number): PrrayPromise<T> {
     return prraypromise(this.then(prray => prray.fill(value, start, end)))
   }
-  sort(callback?: (a: T, b: T) => number): PrrayPromise<T> {
-    return prraypromise(this.then(prray => prray.sort(callback)))
+
+  sort(func?: (a: T, b: T) => number): PrrayPromise<T> {
+    return prraypromise(this.then(prray => prray.sort(func)))
   }
+
   concat(...items: ConcatArray<T>[]): PrrayPromise<T>
   concat(...items: (ConcatArray<T> | T)[]): PrrayPromise<T>
   concat(...items: any[]): PrrayPromise<T> {
     return prraypromise(this.then(prray => prray.concat(...items)))
   }
+
   copyWithin(target: number, start?: number, end?: number) {
     return prraypromise(this.then(prray => prray.copyWithin(target, start, end)))
   }
+
   toString(): Promise<string> {
     return this.then(prray => prray.toString())
   }
+
   toLocaleString(): Promise<string> {
     return this.then(prray => prray.toLocaleString())
   }
+
   forEach(callback: (currentValue: T, index: number, prray: Prray<T>) => any): Promise<void> {
     return this.then(prray => prray.forEach(callback))
   }
+
   pop(): Promise<T> {
     return this.then(prray => prray.pop())
   }
+
   push(...items: T[]): Promise<number> {
     return this.then(prray => prray.push(...items))
   }
+
   reverse(): PrrayPromise<T> {
     return prraypromise(this.then(prray => prray.reverse()))
   }
+
   reduce(callback: (accumulator: T, currentValue: T, index: number, array: Prray<T>) => T): Promise<T>
   reduce<U>(
     callback: (accumulator: U, currentValue: T, index: number, array: Prray<T>) => U,
@@ -141,6 +177,7 @@ class PrrayPromise<T> extends Promise<Prray<T>> {
     // Why? If pass parameter initialValue as undefined, the initial value will be undefined instead array[0] actually :(
     return this.then(prray => (arguments.length >= 2 ? prray.reduce(callback, initialValue) : prray.reduce(callback)))
   }
+
   reduceRight(callback: (accumulator: T, currentValue: T, index: number, array: Prray<T>) => T): Promise<T>
   reduceRight<U>(
     callback: (accumulator: U, currentValue: T, index: number, array: Prray<T>) => U,
@@ -155,21 +192,26 @@ class PrrayPromise<T> extends Promise<Prray<T>> {
       arguments.length >= 2 ? prray.reduceRight(callback, initialValue) : prray.reduceRight(callback),
     )
   }
+
   shift(): Promise<T> {
     return this.then(prray => prray.shift())
   }
+
   unshift(...items: T[]): Promise<number> {
     return this.then(prray => prray.unshift(...items))
   }
+
   splice(start: number, deleteCount?: number, ...items: T[]): PrrayPromise<T> {
     // Why? If pass parameter deleteCount as undefined directly, the delete count will be zero actually :(
     return prraypromise(
       this.then(prray => (arguments.length >= 2 ? prray.splice(start, deleteCount, ...items) : prray.splice(start))),
     )
   }
+
   toArray(): Promise<T[]> {
     return this.then(prray => prray.toArray())
   }
+
 }
 
 function prraypromise<T>(promise: Promise<Prray<T>>) {
